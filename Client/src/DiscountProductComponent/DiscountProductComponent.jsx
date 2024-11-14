50093a0cae2f05985e3d39a4b71ba5d2ae070dbb
Revenko Artem <copies.everywhere@gmail.com>
2024-11-14 22:57:57 +0200

    refactoring

--- Client/src/DiscountProductComponent/DiscountProductComponent.jsx
+++ Client/src/DiscountProductComponent/DiscountProductComponent.jsx
@@ -1,107 +1,82 @@
-import { FunctionComponent } from 'react';
+import { useState, useEffect } from 'react';
 import styles from './DiscountProductComponent.module.css';
 
-
 const DiscountProductComponent = () => {
-  	return (
-    		<div className={styles.parent}>
-      			<div className={styles.div}>Знижки</div>
-      			<div className={styles.frameParent}>
-        				<div className={styles.frameWrapper}>
-          					<div className={styles.frameGroup}>
-            						<img className={styles.frameChild} alt="" src="Frame 1831.png" />
-            						<div className={styles.hogwartsLegacyParent}>
-              							<div className={styles.hogwartsLegacy}>Hogwarts Legacy</div>
-              							<div className={styles.frameContainer}>
-                								<div className={styles.groupParent}>
-                  									<div className={styles.vectorParent}>
-                    										<img className={styles.groupChild} alt="" src="Vector 1.svg" />
-                    										<div className={styles.div1}>1 599₴</div>
-                  									</div>
-                  									<div className={styles.div2}>479₴</div>
-                								</div>
-                								<img className={styles.linearShoppingEcommerce} alt="" src="Linear / Shopping, Ecommerce / Cart Large Minimalistic.svg" />
-              							</div>
-            						</div>
-          					</div>
-        				</div>
-        				<div className={styles.frameWrapper}>
-          					<div className={styles.frameGroup}>
-            						<div className={styles.image109Parent}>
-              							<img className={styles.image109Icon} alt="" src="image 109.png" />
-              							<img className={styles.image110Icon} alt="" src="image 110.png" />
-            						</div>
-            						<div className={styles.hogwartsLegacyParent}>
-              							<div className={styles.redDeadRedemption}>Red Dead Redemption 2</div>
-              							<div className={styles.frameContainer}>
-                								<div className={styles.frameWrapper1}>
-                  									<div className={styles.groupParent}>
-                    										<div className={styles.vectorGroup}>
-                      											<img className={styles.groupChild} alt="" src="Vector 1.svg" />
-                      											<div className={styles.div1}>1 499₴</div>
-                    										</div>
-                    										<div className={styles.div2}>479₴</div>
-                  									</div>
-                								</div>
-                								<img className={styles.linearShoppingEcommerce} alt="" src="Linear / Shopping, Ecommerce / Cart Large Minimalistic.svg" />
-              							</div>
-            						</div>
-          					</div>
-        				</div>
-        				<div className={styles.frameWrapper}>
-          					<div className={styles.frameGroup}>
-            						<div className={styles.image109Parent}>
-              							<img className={styles.image109Icon} alt="" src="image 109.png" />
-              							<img className={styles.imageIcon} alt="" src="image.png" />
-            						</div>
-            						<div className={styles.hogwartsLegacyParent}>
-              							<div className={styles.theLastOf}>The Last of Us Part I</div>
-              							<div className={styles.frameContainer}>
-                								<div className={styles.frameWrapper1}>
-                  									<div className={styles.groupParent}>
-                    										<div className={styles.vectorContainer}>
-                      											<img className={styles.groupChild} alt="" src="Vector 1.svg" />
-                      											<div className={styles.div1}>1 699₴</div>
-                    										</div>
-                    										<div className={styles.div2}>1000₴</div>
-                  									</div>
-                								</div>
-                								<img className={styles.linearShoppingEcommerce} alt="" src="Linear / Shopping, Ecommerce / Cart Large Minimalistic.svg" />
-              							</div>
-            						</div>
-          					</div>
-        				</div>
-        				<div className={styles.frameWrapper}>
-          					<div className={styles.frameParent5}>
-            						<div className={styles.image109Parent}>
-              							<img className={styles.image109Icon} alt="" src="image 109.png" />
-              							<img className={styles.imageIcon1} alt="" src="image.png" />
-            						</div>
-            						<div className={styles.strayParent}>
-              							<div className={styles.redDeadRedemption}>Stray</div>
-              							<div className={styles.frameContainer}>
-                								<div className={styles.frameWrapper1}>
-                  									<div className={styles.groupParent}>
-                    										<div className={styles.groupDiv}>
-                      											<img className={styles.vectorIcon} alt="" src="Vector 1.svg" />
-                      											<div className={styles.div1}>599₴</div>
-                    										</div>
-                    										<div className={styles.div2}>359₴</div>
-                  									</div>
-                								</div>
-                								<img className={styles.linearShoppingEcommerce} alt="" src="Linear / Shopping, Ecommerce / Cart Large Minimalistic.svg" />
-              							</div>
-            						</div>
-          					</div>
-        				</div>
-      			</div>
-      			<div className={styles.buttondefaulttextButtonlar}>
-        				<div className={styles.hogwartsLegacy}>Показати більше</div>
-        				<div className={styles.vectorWrapper}>
-          					<img className={styles.vectorIcon1} alt="" src="Vector.svg" />
-        				</div>
-      			</div>
-    		</div>);
+    const [discountedGames, setDiscountedGames] = useState([]);
+
+    useEffect(() => {
+        // Моковые данные со скидками
+        const mockDiscountData = [
+            {
+                id: 1,
+                name: "Hogwarts Legacy",
+                originalPrice: "1 599₴",
+                discountedPrice: "479₴",
+                imageUrl: "HogwartsLegacy.png"
+            },
+            {
+                id: 2,
+                name: "Red Dead Redemption 2",
+                originalPrice: "1 499₴",
+                discountedPrice: "479₴",
+                imageUrl: "RDR2.png"
+            },
+            {
+                id: 3,
+                name: "The Last of Us Part I",
+                originalPrice: "1 699₴",
+                discountedPrice: "1 000₴",
+                imageUrl: "TLOU1.png"
+            },
+            {
+                id: 4,
+                name: "Stray",
+                originalPrice: "599₴",
+                discountedPrice: "359₴",
+                imageUrl: "Stray.png"
+            }
+        ];
+
+        // Имитация задержки загрузки данных
+        setTimeout(() => {
+            setDiscountedGames(mockDiscountData);
+        }, 1000); // Задержка для демонстрации
+
+    }, []);
+
+    return (
+        <div className={styles.parent}>
+            <div className={styles.div}>Знижки</div>
+            <div className={styles.frameParent}>
+                {discountedGames.map(game => (
+                    <div key={game.id} className={styles.frameWrapper}>
+                        <div className={styles.frameGroup}>
+                            <img className={styles.frameChild} alt="" src={game.imageUrl} />
+                            <div className={styles.hogwartsLegacyParent}>
+                                <div className={styles.hogwartsLegacy}>{game.name}</div>
+                                <div className={styles.frameContainer}>
+                                    <div className={styles.groupParent}>
+                                        <div className={styles.vectorParent}>
+                                            <img className={styles.groupChild} alt="" src="Vector 1.svg" />
+                                            <div className={styles.div1}>{game.originalPrice}</div>
+                                        </div>
+                                        <div className={styles.div2}>{game.discountedPrice}</div>
+                                    </div>
+                                    <img className={styles.linearShoppingEcommerce} alt="" src="Cart Large Minimalistic.svg" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div className={styles.buttondefaulttextButtonlar}>
+                <div className={styles.hogwartsLegacy}>Показати більше</div>
+                <div className={styles.vectorWrapper}>
+                    <img className={styles.vectorIcon1} alt="" src="Vector.svg" />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default DiscountProductComponent;
--- Client/src/PopularProductComponenet/PopularProductComponent.jsx
+++ Client/src/PopularProductComponenet/PopularProductComponent.jsx
@@ -1,66 +1,72 @@
-import { FunctionComponent } from 'react';
-import styles from './PopularProductComponent.module.css'
-
+import { useState, useEffect } from 'react';
+import styles from './PopularProductComponent.module.css';
 
 const PopularProductComponent = () => {
-  	return (
-    		<div className={styles.parent}>
-      			<div className={styles.div}>Популярні ігри</div>
-      			<div className={styles.frameParent}>
-        				<div className={styles.frameWrapper}>
-          					<div className={styles.frameGroup}>
-            						<img className={styles.frameChild} alt="" src="Frame 1831.png" />
-            						<div className={styles.callOfDutyBlackOps6Parent}>
-              							<div className={styles.callOfDuty}>Call of Duty®: Black Ops 6</div>
-              							<div className={styles.group}>
-                								<div className={styles.div1}>2 999₴</div>
-                								<img className={styles.linearShoppingEcommerce} alt="" src="Linear / Shopping, Ecommerce / Cart Large Minimalistic.svg" />
-              							</div>
-            						</div>
-          					</div>
-        				</div>
-        				<div className={styles.frameWrapper}>
-          					<div className={styles.frameGroup}>
-            						<img className={styles.frameChild} alt="" src="Frame 1831.png" />
-            						<div className={styles.callOfDutyBlackOps6Parent}>
-              							<div className={styles.callOfDuty}>HELLDIVERS™ 2</div>
-              							<div className={styles.group}>
-                								<div className={styles.div1}>1 199₴</div>
-                								<img className={styles.linearShoppingEcommerce} alt="" src="Linear / Shopping, Ecommerce / Cart Large Minimalistic.svg" />
-              							</div>
-            						</div>
-          					</div>
-        				</div>
-        				<div className={styles.frameWrapper}>
-          					<div className={styles.frameGroup}>
-            						<img className={styles.frameChild} alt="" src="Frame 1831.png" />
-            						<div className={styles.callOfDutyBlackOps6Parent}>
-              							<div className={styles.callOfDuty}>Cyberpunk 2077</div>
-              							<div className={styles.group}>
-                								<div className={styles.div1}>1 648₴</div>
-                								<img className={styles.linearShoppingEcommerce} alt="" src="Linear / Shopping, Ecommerce / Cart Large Minimalistic.svg" />
-              							</div>
-            						</div>
-          					</div>
-        				</div>
-        				<div className={styles.frameGroup}>
-          					<img className={styles.frameChild} alt="" src="Frame 1831.png" />
-          					<div className={styles.callOfDutyBlackOps6Parent}>
-            						<div className={styles.callOfDuty}>Resident Evil 4</div>
-            						<div className={styles.group}>
-              							<div className={styles.div1}>1 699₴</div>
-              							<img className={styles.linearShoppingEcommerce} alt="" src="Linear / Shopping, Ecommerce / Cart Large Minimalistic.svg" />
-            						</div>
-          					</div>
-        				</div>
-      			</div>
-      			<div className={styles.buttondefaulttextButtonlar}>
-        				<div className={styles.callOfDuty}>Показати більше</div>
-        				<div className={styles.vectorWrapper}>
-          					<img className={styles.vectorIcon} alt="" src="Vector.svg" />
-        				</div>
-      			</div>
-    		</div>);
+    const [games, setGames] = useState([]);
+
+    useEffect(() => {
+        // Заглушка данных
+        const mockData = [
+            {
+                id: 1,
+                name: "Call of Duty®: Black Ops 6",
+                price: "2 999₴",
+                imageUrl: "CoDBO6.png"
+            },
+            {
+                id: 2,
+                name: "HELLDIVERS™ 2",
+                price: "1 199₴",
+                imageUrl: "helldivers2.png"
+            },
+            {
+                id: 3,
+                name: "Cyberpunk 2077",
+                price: "1 648₴",
+                imageUrl: "CyberPunk2077.png"
+            },
+            {
+                id: 4,
+                name: "Resident Evil 4",
+                price: "1 699₴",
+                imageUrl: "ResidentEvil.png"
+            }
+        ];
+
+        // Имитация задержки для загрузки данных
+        setTimeout(() => {
+            setGames(mockData);
+        }, 1000); // Задержка в 1 секунду для демонстрации
+
+    }, []);
+
+    return (
+        <div className={styles.parent}>
+            <div className={styles.div}>Популярні ігри</div>
+            <div className={styles.frameParent}>
+                {games.map(game => (
+                    <div key={game.id} className={styles.frameWrapper}>
+                        <div className={styles.frameGroup}>
+                            <img className={styles.frameChild} alt="" src={game.imageUrl} />
+                            <div className={styles.callOfDutyBlackOps6Parent}>
+                                <div className={styles.callOfDuty}>{game.name}</div>
+                                <div className={styles.group}>
+                                    <div className={styles.div1}>{game.price}</div>
+                                    <img className={styles.linearShoppingEcommerce} alt="" src="Cart Large Minimalistic.svg" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div className={styles.buttondefaulttextButtonlar}>
+                <div className={styles.callOfDuty}>Показати більше</div>
+                <div className={styles.vectorWrapper}>
+                    <img className={styles.vectorIcon} alt="" src="Vector.svg" />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default PopularProductComponent;
